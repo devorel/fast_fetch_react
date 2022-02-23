8f8ad99158730b1360bb8edecda3e97eb918a826
orel <bayorel@gmail.com>
2022-02-23 14:16:02 +0200

    fix bug

--- FastFetch.js
+++ FastFetch.js
@@ -2,20 +2,28 @@ import React, {useEffect, useState} from 'react';
 
 function FastFetch(url, options, expiration) {
 
-    const urlOrginal=url.toString();
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
 
+    function addTime(url) {
+        url = new URL(url);
+        url.searchParams.append('t', (new Date()).getTime());
+        return url.toString();
+    }
+
     useEffect(() => {
+        if (loading) {
+            return;
+        }
         const time = (new Date()).getTime();
         expiration = expiration || 0;
         setLoading(true);
 
-        if (expiration && window.localStorage.getItem(url.toString()) != null) {
+        if (expiration && window.localStorage.getItem(url) != null) {
             // if (expiration && window.sessionStorage.getItem(url.toString()) != null) {
             // let data = JSON.parse(window.sessionStorage.getItem(url.toString()));
-            let data = JSON.parse(window.localStorage.getItem(url.toString()));
+            let data = JSON.parse(window.localStorage.getItem(url));
             if (time < (data.timestamp + expiration)) {
                 setData(data.data);
                 setError(null);
@@ -24,25 +32,25 @@ function FastFetch(url, options, expiration) {
 
             }
         }
-        url = new URL(url);
-        url.searchParams.append('t', time);
-        fetch(url.toString(), options)
+
+        fetch(addTime(url), options)
             .then(res => res.json())
             .then(data => {
                 setData(data);
+                setLoading(false);
                 if (expiration) {
-                    // window.sessionStorage.setItem(urlOrginal, JSON.stringify({timestamp: time, data: data}));
-                    window.localStorage.setItem(urlOrginal.toString(), JSON.stringify({timestamp: time, data: data}));
+                    // window.sessionStorage.setItem(url, JSON.stringify({timestamp: time, data: data}));
+                    window.localStorage.setItem(url, JSON.stringify({timestamp: time, data: data}));
                 }
                 setError(null);
 
             })
             .catch((error) => {
                 setError(error);
-                setData(null);
+                // setData(null);
 
-            }).finally(() => setLoading(false))
-    }, [url, options]);
+            });
+    }, [url]);
 
     return [loading, error, data]
 }
